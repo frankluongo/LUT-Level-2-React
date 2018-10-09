cdf2c402d64ab50106b0f49bfcde2841b9695794
Frank Luongo <frank.luongo@revzilla.com>
2018-10-09 12:27:49 -0400

    13: Updating Context

--- src/User.js
+++ src/User.js
@@ -11,6 +11,7 @@ export default class User extends Component {
             <h1>User Info</h1>
             <h3>{context.user.name}</h3>
             <p>{context.user.email}</p>
+            <button onClick={context.logout}>Logout</button>
           </div>
         )}
       </UserContext.Consumer>
